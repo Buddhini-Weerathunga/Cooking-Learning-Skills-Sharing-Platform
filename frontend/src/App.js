ab4e9e0fc8faa2b976d223e2a252e20b08860ba3
IT22251428 <it22251428@my.sliit.lk>
2025-05-10 10:36:52 +0530

    Backend Changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,32 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import InstructorDashboard from "./Pages/Instructor";
-import CourseForm from "./Pages/CourseForm";
-import CourseList from "./Pages/CourseList";
-import CourseDetail from "./Pages/CourseDetail";
-import EnrollForm from "./Pages/EnrollForm";
-import Course from "./Pages/course";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import EnrollForm from "./components/EnrollForm";
+import EnrollmentList from "./components/EnrollmentList";
+
+import HomePage from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import CourseView from "./components/CourseView";
+import UpdateCourse from "./components/UpdateCourse";
+import CreateCOurse from "./components/createcourse";
+import View from "./components/view";
+import InstructorLogin from "./pages/instructorLogin";
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
-        <Route path="/" element={<InstructorDashboard />} />
-        <Route path="/create" element={<CourseForm />} />
-        <Route path="/edit/:id" element={<CourseForm />} />
-        <Route path="/courses" element={<CourseList />} />
-        <Route path="/courses/:id" element={<CourseDetail />} />
-        <Route path="/enroll/:id" element={<EnrollForm />} />
-        <Route path="/co" element={<Course />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/courses" element={<CourseView />} />
+        <Route path="/course/create" element={<CreateCOurse />} />
+        <Route path="/instructor/login" element={<InstructorLogin />} />
+        <Route path="/course/view/:id" element={<View />} />
+        <Route path="/course/update/:id" element={<UpdateCourse />} />
+        <Route path="/enroll" element={<EnrollForm />} />
+        <Route path="/enroll/list" element={<EnrollmentList />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
